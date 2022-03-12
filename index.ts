f5e40ba44512cb76435f48fd53fdeacd0d974452
claritypotion <fellaemail@gmail.com>
2022-03-13 00:27:19 +0200

    handle label delete; handle lable update

--- index.ts
+++ index.ts
@@ -1,27 +1,41 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 const owner = github.context.payload.organization.login;
-const token = core.getInput('token');
-const team = core.getInput('team');
+const token = core.getInput("token");
+const team = core.getInput("team");
 const octokit = github.getOctokit(token);
+const { name: currentRepo } = github.context.payload.repository;
 
 console.log({ eventName: github.context.eventName });
 enum LabelAction {
-  Created = 'created',
-  Deleted = 'deleted',
+  Created = "created",
+  Deleted = "deleted",
+  Edited = "edited",
 }
 
 async function handleLabelEvent() {
-  const repos = (await octokit.rest.teams.listReposInOrg({
-     org: owner,
-     team_slug: team,
-   })).data?.map(({ name }) => name);
-  console.log({ repos });
-  const { action, label } = github.context.payload; 
+  const repos = (
+    await octokit.rest.teams.listReposInOrg({
+      org: owner,
+      team_slug: team,
+    })
+  ).data
+    ?.map(({ name }) => name)
+    .filter((name) => name != currentRepo);
+
+  const { action, label, changes } = github.context.payload;
   if (action === LabelAction.Created) {
-    console.log('Creating labels', label);
+    console.log("Creating labels", label);
     createLabel(octokit, repos, label);
+  } else if (action === LabelAction.Deleted) {
+    console.log("Deleting labels", label);
+    deleteLabel(octokit, repos, label);
+  } else if (action === LabelAction.Edited) {
+    console.log("Eiditing labels", label);
+    const from = changes?.name?.from;
+    if (!from) return;
+    editLabel(octokit, repos, label, from);
   }
 }
 
@@ -32,28 +46,69 @@ interface Label {
 }
 
 async function createLabel(
-   octokit: ReturnType<typeof github.getOctokit>,
-   repos: string[],
-   label: Label 
+  octokit: ReturnType<typeof github.getOctokit>,
+  repos: string[],
+  label: Label
 ) {
- const { name, color, description } = label;
- await Promise.allSettled(repos.map(repo => 
-   octokit.rest.issues.createLabel({
-     owner,
-     repo,
-     name,
-     color,
-     description,
-   })));  
+  const { name, color, description } = label;
+  await Promise.allSettled(
+    repos.map((repo) =>
+      octokit.rest.issues.createLabel({
+        owner,
+        repo,
+        name,
+        color,
+        description,
+      })
+    )
+  );
+}
+
+async function deleteLabel(
+  octokit: ReturnType<typeof github.getOctokit>,
+  repos: string[],
+  label: Label
+) {
+  const { name } = label;
+  await Promise.allSettled(
+    repos.map((repo) =>
+      octokit.rest.issues.deleteLabel({
+        owner,
+        repo,
+        name,
+      })
+    )
+  );
+}
+
+async function editLabel(
+  octokit: ReturnType<typeof github.getOctokit>,
+  repos: string[],
+  label: Label,
+  from: string
+) {
+  const { name, description, color } = label;
+  await Promise.allSettled(
+    repos.map((repo) =>
+      octokit.rest.issues.updateLabel({
+        owner,
+        repo,
+        name: from,
+        new_name: name,
+        description,
+        color,
+      })
+    )
+  );
 }
 
 async function main() {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
   try {
     if (github.context.payload.label) {
-      console.log('Handling label event');
+      console.log("Handling label event");
       handleLabelEvent();
     }
   } catch (err) {
